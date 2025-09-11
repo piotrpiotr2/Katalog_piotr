e79484cd3c3cd4c97f10a01f2cc650987d49a5cb
Szymon Mirek <piotrekgtpll@gmail.com>
2025-09-11 12:44:48 +0200

    Poprawy cs sniffer

--- app/src/Dto/AlbumListFiltersDto.php
+++ app/src/Dto/AlbumListFiltersDto.php
@@ -17,6 +17,7 @@ class AlbumListFiltersDto
     /**
      * Constructor.
      *
+     * @param Category|null $category Category entity
      * @param Tag|null      $tag      Tag entity
      */
     public function __construct(public readonly ?Category $category, public readonly ?Tag $tag)
